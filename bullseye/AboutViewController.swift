943d214a4fe429c430a03b419591bf7d3c738c9d
Neil Houselander <neilhouselander@sky.com>
2017-10-21 18:50:32 +0100

    added a web view for the instructions page, tidied up main page labels

--- bullseye/AboutViewController.swift
+++ bullseye/AboutViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webview: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            
+            if let htmlData = try? Data(contentsOf: url) {
+                
+                let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webview.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+            }
+        }
         
     }
 
